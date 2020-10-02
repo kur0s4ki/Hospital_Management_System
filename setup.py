b0e17f44ec5dc9c0d3b88cacdf6129999ab03800
kur0s4ki <54065821+kur0s4ki@users.noreply.github.com>
2020-10-02 14:51:05 +0100

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Kur0s4ki',
+    author_email='mohamedelamine.aghrib@usmba.ac.ma',
+    description='HospitalManagementSystem'
+)
